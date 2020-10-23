752cca87fdd2730a61942c1c6743eaaa44e8bf07
good3n <good3n@gmail.com>
2020-10-23 16:32:52 -0400

    add discord link to nav

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -18,10 +18,25 @@ export const StyledHeader = styled.header`
 export const Nav = styled.ul`
   margin: 0 0 0 auto;
   padding: 0;
+  display: flex;
+  align-items: center;
 
   li {
     display: block;
     margin: 0;
+    font-weight: 600;
+    position: relative;
+
+    &:hover:not(:last-of-type):after {
+      content: '';
+      background: var(--color-ch-purple);
+      height: 3px;
+      width: 100%;
+      position: absolute;
+      right: 0;
+      bottom: -5px;
+      left: 0;
+    }
   }
 
   a {
@@ -29,6 +44,17 @@ export const Nav = styled.ul`
 
     &.is-active {
       color: var(--color-ch-purple);
+    }
+  }
+
+  svg {
+    width: 24px;
+    margin: 3px 0 0 30px;
+
+    &:hover {
+      path {
+        fill: var(--color-ch-purple);
+      }
     }
   }
 `
