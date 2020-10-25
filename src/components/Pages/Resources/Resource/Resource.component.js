fec23c05980c2671a4433fcdd07f35988f0b7eb6
good3n <good3n@gmail.com>
2020-10-24 23:01:39 -0400

    make resource links nofollow

--- src/components/Pages/Resources/Resource/Resource.component.js
+++ src/components/Pages/Resources/Resource/Resource.component.js
@@ -5,7 +5,7 @@ const Resource = props => {
   const { name, desc, link } = props
   return (
     <StyledResource>
-      <Title href={link} id={name}>
+      <Title href={link} id={name} rel="nofollow">
         {name}
       </Title>
       <Description>{desc}</Description>
