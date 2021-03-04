52bd90afb793f9e94fef5c4768bc80d240e1c6fa
Tom <good3n@gmail.com>
2021-03-04 00:56:06 -0500

    Add popup functionality

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Popup from 'reactjs-popup'
 
 export const StyledHeader = styled.header`
   padding: 100px 0 50px;
@@ -50,10 +51,54 @@ export const Nav = styled.ul`
   svg {
     width: 24px;
     margin: 3px 0 0 30px;
+    cursor: pointer;
 
     &:hover {
       path {
         fill: var(--color-ch-purple);
+      }
+    }
+  }
+`
+
+export const StyledPopup = styled(Popup)`
+  &-overlay {
+    background-color: rgba(94, 124, 231, 0.8);
+  }
+
+  &-content {
+    background: var(--bg-color);
+    border-radius: 10px;
+    padding: 40px;
+    font-size: 1rem;
+
+    p {
+      margin: 0;
+      font-weight: bold;
+
+      + div {
+        margin-top: 20px;
+        display: flex;
+
+        a {
+          color: var(--color-ch-green);
+          text-decoration: none;
+          font-weight: bold;
+          margin-left: 20px;
+          order: 2;
+        }
+
+        button {
+          background: none;
+          border: none;
+          font-weight: bold;
+          padding: 0;
+          box-shadow: 0;
+          color: var(--color-ch-green);
+          cursor: pointer;
+          order: 1;
+          margin-left: auto;
+        }
       }
     }
   }
