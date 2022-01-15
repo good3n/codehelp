5de45c6ebf9fa7fc5f8f5db08104df3e56178e45
Tom Gooden <good3n@gmail.com>
2022-01-15 10:04:05 -0500

    Remove react sticky

--- src/components/ResourcesSidebar/ResourcesSidebar.component.js
+++ src/components/ResourcesSidebar/ResourcesSidebar.component.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import StickyBox from 'react-sticky-box'
 import Resources from '../../data/resources.json'
 import { AnchorLink } from 'gatsby-plugin-anchor-links'
 import { Sidebar, Heading } from './ResourcesSidebar.styles'
@@ -9,27 +8,25 @@ const ResourcesSidebar = () => {
 
   return (
     <Sidebar>
-      <StickyBox offsetTop={50} offsetBottom={50}>
-        {resources.map(({ label }) => {
-          return (
-            <>
-              <Heading key={label.id}>{label.name}</Heading>
-              {label.categories.map(({ category }) => {
-                return (
-                  <li key={category.id}>
-                    <AnchorLink
-                      to={`/resources#${category.id}`}
-                      title={category.name}
-                    >
-                      {category.name}
-                    </AnchorLink>
-                  </li>
-                )
-              })}
-            </>
-          )
-        })}
-      </StickyBox>
+      {resources.map(({ label }) => {
+        return (
+          <>
+            <Heading key={label.id}>{label.name}</Heading>
+            {label.categories.map(({ category }) => {
+              return (
+                <li key={category.id}>
+                  <AnchorLink
+                    to={`/resources#${category.id}`}
+                    title={category.name}
+                  >
+                    {category.name}
+                  </AnchorLink>
+                </li>
+              )
+            })}
+          </>
+        )
+      })}
     </Sidebar>
   )
 }
