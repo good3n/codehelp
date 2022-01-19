80c3065cf91cf7f0a05b2ad2864dc673fb039546
Tom Gooden <good3n@gmail.com>
2022-01-19 09:21:04 -0500

    Change logo size for mobile

--- src/components/HeaderNew/HeaderNew.styles.js
+++ src/components/HeaderNew/HeaderNew.styles.js
@@ -8,17 +8,31 @@ export const StyledHeader = styled.header`
   position: relative;
   z-index: 999;
 
+  @media (max-width: 991px) {
+    grid-template-columns: 60px 1fr;
+  }
+
   > a:first-of-type {
     border: 2px solid rgba(255, 255, 255, 0.15);
     border-radius: 10px 10px 0 10px;
     height: 80px;
     display: flex;
 
+    @media (max-width: 991px) {
+      height: 60px;
+    }
+
     svg {
       height: 76px;
       margin: 0 -11px;
       position: relative;
       top: -3px;
+
+      @media (max-width: 991px) {
+        top: 0;
+        height: 55px;
+        margin: 0 -5px;
+      }
     }
   }
 
